9edd2ddc97ad1c36f9929889545fcc0491c46eb9
fine <92whdtjs@gmail.com>
2024-08-30 15:52:51 +0900

    [Multiplatform] Project Setting

---
+++ FrontyardBirds/Multiplatform/FrontyardBirdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrontyardBirdsApp.swift
+//  FrontyardBirds
+//
+//  Created by 이종선 on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct FrontyardBirdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
